ec66b3aaca0389b6c2ccd0ac21725c5801357ddb
anikashc <akh.chakraborty11@gmail.com>
2021-01-16 14:43:27 +0530

    Added Basic Styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import  {Container} from 'react-bootstrap'
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Header />
+      
+      <main>
+        <Container>
+        <h1>Welcome to Sero</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }

0b04ca526e7e010df07fd93c2d8f84cdb565bd35
amritx8 <amritx8@gmail.com>
2021-01-22 15:59:57 +0530

    first commit

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <header>
-            Header
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">Sero</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </header>
     )
 }

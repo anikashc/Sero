0ecdf3ebd9aa056c839c1a92f53e4720a58e30b7
anikashc <akh.chakraborty11@gmail.com>
2021-01-27 13:08:09 +0530

    Fetching from db

--- client/src/Screens/Menu.js
+++ client/src/Screens/Menu.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Item from '../Components/Item';
 import item from '../item';
-import eateries from '../eateries';
+import axios from 'axios'
+//import eateries from '../eateries';
 
 const Menu = ({match}) => {
     // match.params.id or match.params. anything that is in the url
-    const eatery = eateries.find(p=>p._id===match.params.id)
+    //const eatery = eateries.find(p=>p._id===match.params.id)
+    const [eatery,setEatery] = useState({})
+    useEffect(()=>{
+        const fetchEatery = async () =>{
+            const {data}= await axios.get(`/api/eateries/${match.params.id}`)
+            setEatery(data) 
+        }
+        fetchEatery ()
+    },[])
     return (
         <div>
             <Link className="btn btn-dark my-3" to="/">Go Back</Link>

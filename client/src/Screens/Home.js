9b56cb44535d630047119cd7c2749a58f2de70a5
anikashc <akh.chakraborty11@gmail.com>
2021-01-27 15:34:26 +0530

    Added redux

--- client/src/Screens/Home.js
+++ client/src/Screens/Home.js
@@ -1,26 +1,33 @@
-import axios from 'axios';
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import { Row, Col } from 'react-bootstrap';
-
-
+import {useDispatch, useSelector} from 'react-redux'
 import Eatery from '../Components/Eatery';
-//import eateries from '../eateries';
+import Loader from '../Components/Loader';
+import Message from '../Components/Message';
+import { listEateries} from '../actions/eateryActions'
 function Home() {
-    const [eateries,setEateries] = useState([])
+    const dispatch = useDispatch()
 
+    const  eateryList = useSelector(state => state.eateryList) // call whatever you call in the store
+    const {loading, error, eateries} = eateryList
     useEffect(()=>{
-        const fetchEateries = async () =>{
-            const {data}= await axios.get('/api/eateries')
-            setEateries(data) 
-        }
-        fetchEateries()
-    },[])
+        dispatch(listEateries())
+    },[dispatch])
+
+    
 
     return (
         <>
             <center>
                 <h2>Trending Eateries</h2>    
             </center>
+            {loading? (
+                <Loader /> 
+                )
+            :   error ?(
+                <Message variant='danger'>{error}</Message>
+                ) 
+            : (
             <Row>
                 {eateries.map(eatery => (
                     <Col key={eatery._id} sm={12} md={6} lg={4} xl={3}>
@@ -28,6 +35,10 @@ function Home() {
                     </Col>
                 ))}
             </Row>
+            )
+            
+            }
+            
         </>
     );
 }
--- client/src/Screens/Menu.js
+++ client/src/Screens/Menu.js
@@ -1,48 +1,72 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Item from '../Components/Item';
 import item from '../item';
-import axios from 'axios'
+import { listEateryDetails} from '../actions/eateryActions'
+import Loader from '../Components/Loader';
+import Message from '../Components/Message';
+//import axios from 'axios'
 //import eateries from '../eateries';
 
 const Menu = ({match}) => {
     // match.params.id or match.params. anything that is in the url
     //const eatery = eateries.find(p=>p._id===match.params.id)
-    const [eatery,setEatery] = useState({})
+    //const [eatery,setEatery] = useState({})
+    const dispatch = useDispatch()
+    const  eateryDetails = useSelector(state => state.eateryDetails) // call whatever you call in the store
+    const {loading, error, eatery} = eateryDetails
     useEffect(()=>{
-        const fetchEatery = async () =>{
-            const {data}= await axios.get(`/api/eateries/${match.params.id}`)
-            setEatery(data) 
-        }
-        fetchEatery ()
-    },[])
+          dispatch(listEateryDetails(match.params.id))
+    },[dispatch, match])
+    
     return (
         <div>
             <Link className="btn btn-dark my-3" to="/">Go Back</Link>
-            <Row>
-                <Col md={6}>
-                    <Image src={eatery.image} alt={eatery.name } fluid />
-                </Col>
-                <Col md={6}>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                        <h3>{eatery.name}</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        Description: {eatery.description}
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-            </Row>
-            <Row>
-                <h2 className='menu-title py-3'>Menu</h2>
-            </Row>
-            {item.map(item => (
-                <Row key={item._id}>
-                    <Item item={item} />
-                </Row>
-            ))}
+            {loading? (
+                <Loader /> 
+                )
+            :   error ?(
+                <Message variant='danger'>{error}</Message>
+                ) 
+            :   (
+                <>
+                    <Row>
+                        <Col md={6}>
+                            <Image src={eatery.image} alt={eatery.name } fluid />
+                        </Col>
+                        <Col md={6}>
+                            <ListGroup variant='flush'>
+                                <ListGroup.Item>
+                                <h3>{eatery.name}</h3>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                Description: {eatery.description}
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                    </Row>
+                    <Row>
+                    <h2 className='menu-title py-3'>Menu</h2>
+                    </Row>
+                    {item.map(item => (
+                        <Row key={item._id}>
+                            <Item item={item} />
+                        </Row>
+                    ))}
+
+                </>
+
+
+
+                
+            
+                )
+            
+            }
+            
+            
         </div>
     );
 }

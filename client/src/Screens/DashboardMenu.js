394c8f76df7d14740323a11bb31a2aca5e211f05
Vishesh Malhotra <vishesh.malhotra321@gmail.com>
2021-02-22 12:02:53 +0530

    Mapping in DashboardMenu

--- client/src/Screens/DashboardMenu.js
+++ client/src/Screens/DashboardMenu.js
@@ -3,9 +3,22 @@ import { Row, Col, Container, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import DashboardItem from '../Components/DashboardItem';
 
+const groupBy = (objectArray, property) => {
+    return objectArray.reduce((acc, obj) => {
+       const key = obj[property];
+       if (!acc[key]) {
+          acc[key] = [];
+       }
+       // Add object to list for given key's value
+       acc[key].push(obj);
+       return acc;
+    }, {});
+ }
+
 
 function DashboardMenu(props) {
     const {eateryMenu} = props.location.state
+    const menu = groupBy(eateryMenu, 'category')
 
     return (
         <>
@@ -20,9 +33,17 @@ function DashboardMenu(props) {
             <Container className='py-3'>
                 <Row>
 
-                    {eateryMenu.map((item => 
+                    {/* {eateryMenu.map((item => 
                         <Col className='py-3'> <DashboardItem item={item}/> </Col>
-                    ))}
+                    ))} */}
+                    {Object.keys(menu).map((key, i) => 
+                        (
+                            <Col className='py-3' key={i}>
+                                <h3>{key}</h3>
+                                <DashboardItem key={key} menu={menu[key]}/> 
+                            </Col>
+                        )
+                    )}
                 </Row>
             </Container>
         </>

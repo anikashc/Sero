cd441766b2a8a9284af8d08bb7ad80db2e48ae66
Vishesh Malhotra <vishesh.malhotra321@gmail.com>
2021-02-22 12:02:53 +0530

    Mapping in DashboardMenu

--- client/src/Components/DashboardItem.js
+++ client/src/Components/DashboardItem.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { Button, ButtonGroup, Card, Col } from 'react-bootstrap';
+import {Row, Button, ButtonGroup, Card, Col } from 'react-bootstrap';
 
-const DashboardItem = ({item}) => {
+const DashboardItem = ({key, menu}) => {
     return (
-        <Card style={{ height: '5rem', width: '50rem' }}>
-            <Card.Body>
-                <Card.Title> { item.name } </Card.Title>
-            </Card.Body>
-            <ButtonGroup>
-                <Col>
-                <Button variant='warning'>Edit</Button>
-                </Col>
-                <Col>
-                <Button variant='warning'>Edit</Button>
-                </Col>
-            </ButtonGroup>
-        </Card>
+        <>
+            <h2>{key}</h2>
+            {menu.map((item) => {
+                return(
+                    <Row>
+                        <Card style={{ height: '5rem', width: '50rem' }}>
+                            <Card.Body>
+                                <Card.Title> { item.name } </Card.Title>
+                            </Card.Body>
+                            <ButtonGroup>
+                                <Col>
+                                <Button variant='warning'>Edit</Button>
+                                </Col>
+                                <Col>
+                                <Button variant='warning'>Edit</Button>
+                                </Col>
+                            </ButtonGroup>
+                        </Card>
+                    </Row>
+                )
+            })}
+        </>
     );
 }
 
